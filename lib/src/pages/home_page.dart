05983bd235f2de525d5e76c1e8063a092d9a1926
David Galindo <david@peekmexico.com>
2021-11-07 19:06:35 -0600

    Se agrega utilidad para obtener iconos por strings

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,6 @@
-import 'package:componentes/src/providers/menu_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:componentes/src/providers/menu_provider.dart';
+import 'package:componentes/src/utils/icono_string_util.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -16,7 +17,7 @@ class HomePage extends StatelessWidget {
     // print(menuProvider.opciones);
     // menuProvider.cargarData()
     return FutureBuilder(
-      future: menuProvider.cargarData(),
+      future: menuProvider.cargarData(),  // Método que regresa el future
       initialData: [],
       builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
@@ -32,7 +33,7 @@ class HomePage extends StatelessWidget {
     data.forEach((opt) {
       final widgetTemp = ListTile(
         title: Text(opt['texto']),
-        leading: Icon(Icons.account_circle, color: Colors.blue),
+        leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
         onTap: () {},
       );
